27ec384703fe48ad2d43e1ada3aaea455da74fe6
Garion's Blade <yuxuanng@hotmail.com>
2019-02-01 20:39:46 -0600

    closes #3, closes #4, closes #5

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types'
 import './App.css';
 import data from './static/data/products.json'
 import './style.scss'
+import cartImage from './static/bag-icon.png'
 
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cartProducts: []
+            cartProducts: data.products.map((p)=> ({id: p.id, qty: 0})),
+            totalQty: 0
         };
     }
 
     addProduct = (p) => {
-        let cartP = this.state.cartProducts.slice(0)
-        cartP.push(p)
-        this.setState({cartProducts: cartP})
+        let state = this.state
+        let prod = state.cartProducts.find((pd)=> pd.id === p.id)
+        prod.qty = prod.qty+1
+        state.totalQty += 1
+        this.setState(state)
+    }
+
+    removeFromCart = (id) => {
+        let state = this.state
+        let prod = state.cartProducts.find((cp) => cp.id === id)
+        state.totalQty -= prod.qty
+        prod.qty = 0
+        this.setState(state)
     }
 
     render() {
-        console.log(this.state)
-         return (
+        return (
             <div className="App">
                 <ProductList products={data.products} addProduct={this.addProduct}/>
-                <FloatCart cartProducts={this.state.cartProducts}/>
+                <FloatCart cartProducts={this.state.cartProducts} products={data.products} totalQty={this.state.totalQty} removeFromCart={this.removeFromCart}/>
             </div>
         );
     }
@@ -56,7 +66,6 @@ class ProductItem extends Component {
                          title={product.title}/>
                 </div>
                 <ProductDetails product={product}/>
-                <div className="shelf-item__buy-btn">Add to cart</div>
             </div>
         )
     }
@@ -83,22 +92,24 @@ class FloatCart extends Component {
     };
 
     openFloatCart = () => {
-        this.setState({ isOpen: true });
+        this.setState({isOpen: true});
     };
 
     closeFloatCart = () => {
-        this.setState({ isOpen: false });
+        this.setState({isOpen: false});
     };
 
     render() {
-        const cartProducts = this.props.cartProducts;
-        let totalprice = 0
-        const products = cartProducts.map(p => {
-            totalprice += p.price
-            return (
-                <CartProduct product={p} key={p.id} />
-            );
-        });
+        const { cartProducts, products, totalQty, removeFromCart } = this.props;
+        let totalPrice = 0
+        let productList = []
+        cartProducts.forEach((cp)=> {
+            let prod = products.find((p)=>p.id === cp.id)
+            totalPrice += cp.qty * prod.price
+            if (cp.qty > 0) {
+                productList.push(<CartProduct product={prod} qty={cp.qty} removeFromCart={removeFromCart}/>)
+            }
+        })
 
 
         let classes = ['float-cart'];
@@ -121,11 +132,14 @@ class FloatCart extends Component {
 
                 {/* If cart is closed, show bag with quantity of product and open cart action */}
                 {!this.state.isOpen && (
-                    <span
+                    <div>
+                    <img
                         onClick={() => this.openFloatCart()}
-                        className="bag bag--float-cart-closed"
-                    >
-          </span>
+                        src={cartImage}
+                        className="float-cart-closed"
+                    />
+                        <div className="float-cart-closed-quantity">{totalQty}</div>
+                    </div>
                 )}
 
                 <div className="float-cart__content">
@@ -134,10 +148,10 @@ class FloatCart extends Component {
                     </div>
 
                     <div className="float-cart__shelf-container">
-                        {products}
-                        {!products.length && (
+                        {productList}
+                        {(totalQty === 0) && (
                             <p className="shelf-empty">
-                                Add some products in the cart <br />
+                                Add some products in the cart <br/>
                                 :)
                             </p>
                         )}
@@ -147,7 +161,7 @@ class FloatCart extends Component {
                         <div className="sub">SUBTOTAL</div>
                         <div className="sub-price">
                             <p className="sub-price__val">
-                                ${totalprice}
+                                ${totalPrice}
                             </p>
                             <small className="sub-price__installment">
 
@@ -162,12 +176,13 @@ class FloatCart extends Component {
 
 class CartProduct extends Component {
     render() {
-        const product = this.props.product;
+        const { product, qty, removeFromCart } = this.props
 
         const classes = ['shelf-item'];
 
         return (
             <div className={classes.join(' ')}>
+                <div className="shelf-item__del" onClick={() => removeFromCart(product.id)}>x</div>
                 <img
                     classes="shelf-item__thumb"
                     src={require(`./static/data/products/${product.sku}_2.jpg`)}
@@ -176,15 +191,13 @@ class CartProduct extends Component {
                 <div className="shelf-item__details">
                     <p className="title">{product.title}</p>
                     <p className="desc">
-                        {`${product.availableSizes[0]} | ${product.style}`} <br />
-                        Quantity: {product.quantity}
+                        {`${product.availableSizes[0]} | ${product.style}`} <br/>
+                        Quantity: {qty}
                     </p>
                 </div>
                 <div className="shelf-item__price">
                     <p>{`${product.currencyFormat}  ${product.price}`}</p>
                 </div>
-
-                <div className="clearfix" />
             </div>
         );
     }
